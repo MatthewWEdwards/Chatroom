8bae0533ecc6ee420314dbb6823f21d382c0c785
Matthew Edwards <mathew41796@gmail.com>
2016-11-30 04:59:28 -0600

    Forgot to add a file.

---
+++ src/assignment7/ChatRoom.java
@@ -0,0 +1,78 @@
+/* Chat Room ChatRoom.java
+ * EE422C Project 7 submission by
+ * Regan Stehle
+ * rms3762
+ * 16465
+ * Matthew Edwards
+ * mwe295
+ * 16475
+ * Slip days used: <0>
+ * Fall 2016
+ */
+
+package assignment7;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Observable;
+import java.util.Observer;
+import java.util.Set;
+
+public class ChatRoom extends Observable{
+	public String name;
+	public boolean privateChat;
+	private Set<ChatUser> approvedUsers = new HashSet<ChatUser>();
+	public String owner;
+	
+	public ChatRoom(String name, String owner, boolean privateChat){
+		this.name = name;
+		this.privateChat = privateChat;
+		this.owner = owner;
+	}
+	
+	public void getMessage(String message){
+		notifyObservers(message);
+	}
+	
+	public void addUser(ChatUser u){
+		approvedUsers.add(u);
+	}
+	
+	public void removeUser(ChatUser u){
+		approvedUsers.remove(u);
+	}
+	
+	@Override
+	public void addObserver(Observer u){
+		if(!(u instanceof ChatUser)){
+			return;
+		}
+		ChatUser userToAdd = (ChatUser) u;
+		if(!privateChat){
+			super.addObserver(userToAdd);
+		}else{
+			if(approvedUsers.contains(userToAdd)){
+				super.addObserver(userToAdd);
+			}
+		}
+	}
+	
+	@Override
+	public boolean equals(Object r){
+		if(!(r instanceof ChatRoom)){
+			return false;
+		}
+		ChatRoom testRoom = (ChatRoom) r;
+		if(testRoom.name.equals(this.name)){
+			return true;
+		}
+		return false;
+	}
+	
+	@Override
+	public String toString(){
+		return name;
+	}
+	
+
+}
