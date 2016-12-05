1214567601c32ad8f3dea503e627c4cf7173726c
Matthew Edwards <mathew41796@gmail.com>
2016-12-05 10:03:48 -0600

    Pushing serverMain

---
+++ src/assignment7/ServerMain.java
@@ -0,0 +1,19 @@
+/* Chat Room ServerMain.java
+ * EE422C Project 7 submission by
+ * Regan Stehle
+ * rms3762
+ * 16465
+ * Matthew Edwards
+ * mwe295
+ * 16475
+ * Slip days used: <1>
+ * Fall 2016
+ */
+
+package assignment7;
+
+public class ServerMain{
+	public static void main(){
+		ChatServer.main(null);
+	}
+}
